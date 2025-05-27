39e7078a9c49c74292f70528b671aff8c3b6466f
Shayen Ramsahai <shayen.ramsahai@derivco.co.im>
2025-05-27 16:41:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Resume
-Resume of Shayen Ramsahai
+### Description
+
+A simple portfolio template  built with React. 
+
+### [live preview](https://shayenr.github.io/)
+
+[![react portfoiio](src/assets/images/react%20portfolio%20gif.gif)](https://shayenr.github.io/)
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+Get the code
+
+<pre>git clone https://github.com/shayenr/shayenr.github.io.git</pre>
+ 
+Install required dependencies
+
+<pre>yarn install</pre>
+
+
+Start the server
+
+<pre>yarn start</pre>
+
+### More
+
+Modify pages content in  `src/content_option.js`.
+
+### Thanks
+
---
+++ src/content_option.js
@@ -0,0 +1,174 @@
+const logotext = "SHAYEN";
+const meta = {
+    title: "Shayen Ramsahai",
+    description: "I am Shayen Ramsahai _ Engineer _ PhD cadidate in Blockchain adoption _ Currently residing in the Isle of Man",
+};
+
+const introdata = {
+    title: "Shayen Ramsahai",
+    animated: {
+        first: "Web3.0 researcher",
+        second: "Crypto currency and Blockchain specialist",
+        third: "Technology enthusiast",
+    },
+    description: "",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "",
+};
+const worktimeline = [{
+        jobtitle: "IT Engineer",
+        where: "Derivco - Isle of Man",
+        date: "2019-present",
+    },
+    {
+        jobtitle: "Software Quality Engineer",
+        where: "Derivco - South Africa",
+        date: "2016-2019",
+    },
+    {
+        jobtitle: "Quality Assurance",
+        where: "Derivco - South Africa",
+        date: "2013-206",
+    },
+];
+
+const skills = [{
+        name: "Firewall provisioning",
+        value: 90,
+    },
+    {
+        name: "Server provisioning",
+        value: 85,
+    },
+    {
+        name: "SQL admin",
+        value: 80,
+    },
+    {
+        name: "MS Azure DevOps",
+        value: 60,
+    },
+    {
+        name: "F5 loadbalancing",
+        value: 85,
+    },
+     {
+        name: "Monitoring and observability",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Casino back-office management",
+        description: "",
+    },
+    {
+        title: "Product management",
+        description: "",
+    },
+    {
+        title: "Process optimisation",
+        description: "",
+    },
+     {
+        title: "Casino backend infrastructure management",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shayen.ramsahai@gmail.com",
+    YOUR_FONE: "+447624290895",
+    description: "Feel free to email me or contact me on whatsapp for an queries",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
